d8af547f3cf9eaf1a119e19caa6c9efc989ad39d
favouradeleke246-maker <favouradeleke246@gmail.com>
2026-07-16 23:19:07 +0100

    Add new AI and Admin settings to config.py

--- config.py
+++ config.py
@@ -10,3 +10,9 @@ TIKTOK_SESSION = os.getenv('TIKTOK_SESSION', '')
 SMS_GATEWAY_API_KEY = os.getenv('SMS_GATEWAY_API_KEY', '')
 SMS_GATEWAY_URL = os.getenv('SMS_GATEWAY_URL', 'https://api.smsgateway.com/send')
 PLUGIN_DIR = 'modules/plugins'
+
+# New AI & Admin settings
+DEFAULT_AI = os.getenv('DEFAULT_AI', 'groq')
+OLLAMA_URL = os.getenv('OLLAMA_URL', '')
+SUPER_ADMIN_ID = int(os.getenv('SUPER_ADMIN_ID', '0'))
+VIRUSTOTAL_API_KEY = os.getenv('VIRUSTOTAL_API_KEY', '')
