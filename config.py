05cd919c6413f0d9a6137fc86ef43c6c9e2dd9c6
favouradeleke246-maker <favouradeleke246@gmail.com>
2026-07-17 18:10:59 +0100

    Refactor comments for AI and upgrade settings

--- config.py
+++ config.py
@@ -11,16 +11,18 @@ SMS_GATEWAY_API_KEY = os.getenv('SMS_GATEWAY_API_KEY', '')
 SMS_GATEWAY_URL = os.getenv('SMS_GATEWAY_URL', 'https://api.smsgateway.com/send')
 PLUGIN_DIR = 'modules/plugins'
 
-# New AI & Admin settings
+# AI & Admin
 DEFAULT_AI = os.getenv('DEFAULT_AI', 'groq')
 OLLAMA_URL = os.getenv('OLLAMA_URL', '')
 SUPER_ADMIN_ID = int(os.getenv('SUPER_ADMIN_ID', '0'))
 VIRUSTOTAL_API_KEY = os.getenv('VIRUSTOTAL_API_KEY', '')
 PHONE_VALIDATION_API_KEY = os.getenv('PHONE_VALIDATION_API_KEY', '')
 
-# Upgrade 1-20 settings
+# Upgrades
 BITLY_API_KEY = os.getenv('BITLY_API_KEY', '')
 SHODAN_API_KEY = os.getenv('SHODAN_API_KEY', '')
 REDIS_URL = os.getenv('REDIS_URL', '')
 ENABLE_SCHEDULER = os.getenv('ENABLE_SCHEDULER', 'false').lower() == 'true'
 ENCRYPTION_KEY = os.getenv('ENCRYPTION_KEY', '')
+DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY', '')
+DEEPSEEK_BASE_URL = os.getenv('DEEPSEEK_BASE_URL', 'https://api.deepseek.com')
